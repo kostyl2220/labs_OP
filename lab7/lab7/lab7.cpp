e7cda6b4b592d3a377bde15eaa1a1c6983889f21
koyle__ <kostyl2220@ukr.net>
2016-03-13 16:48:23 +0200

    main func and prototypes

---
+++ lab7/lab7/lab7.cpp
@@ -0,0 +1,30 @@
+// lab7.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+void input(char*);
+void count(char*, char[]);
+void out(char[]);
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	char list[256] = {};
+	char *str;
+	input(str);
+	count(str, list);
+	out(list);
+	return 0;
+}
+
+void input(char*){
+
+}
+
+void count(char*, char[]){
+
+}
+
+void out(char[]){
+
+}
