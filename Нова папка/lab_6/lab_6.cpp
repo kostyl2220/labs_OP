5f124234b6c2bee9601516f78f47bac73248b5eb
koyle__ <kostyl2220@ukr.net>
2016-03-13 16:33:11 +0200

    init lab7

---
+++ Нова папка/lab_6/lab_6.cpp
@@ -0,0 +1,46 @@
+// lab_6.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+
+void input(double&, double&);
+int NSD(int, int);
+bool check(double, double);
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	double n, m;
+	input(n, m);
+	printf("NSD is %d\n", NSD((int)n, (int)m));
+	system("pause");
+	return 0;
+}
+
+int NSD(int n, int m){
+	if (n == m) return n;
+	if (n == 0) return m;
+	if (m == 0) return n;
+	if (m == 1 || n == 1) return 1;
+	if (n % 2 == 0 && m % 2 == 0) return 2 * NSD(n / 2, m / 2);
+	if (n % 2 == 0 && m % 2 != 0) return NSD(n / 2, m);
+	if (n % 2 != 0 && m % 2 == 0) return NSD(n, m / 2);
+	return NSD(m, abs(n - m) / 2);
+}
+
+bool check(double n, double m){
+	if (n < 1 || m < 1 ||
+		(int)n != n || (int)m != m){
+		printf("You entered wrong numbers! Try again!\n");
+		return false;
+	}
+	return true;
+}
+
+void input(double &n, double &m) {
+	printf("Enter n and m:\n");
+	do {
+		scanf_s("%lf %lf", &n, &m);
+	} while (!check(n, m));
+}
